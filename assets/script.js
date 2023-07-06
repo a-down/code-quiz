eba09eeaa511820e10fd73b02379727eda769ddb
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-06 15:04:44 -0500

    JS notes

---
+++ assets/script.js
@@ -0,0 +1,54 @@
+// global variables
+
+
+
+
+// global objects (questions) - test this with 2 questions, so it is quicker to run through the whole thing!
+
+objectExample = {
+  choice-a: example,
+  choice-a: example,
+  choice-a: example,
+  choice-a: example,
+  correct-answer: example,
+}
+
+
+
+// add text for page greeting
+
+
+
+
+// add buttton to start the quiz (style all buttons in CSS)
+
+
+
+
+// code that creates elements to add the questions and answers to <main>, runs after the button is clicked
+
+
+
+
+// eventListener for main to determine which answer they click on 
+
+    // If the answer they clicked on is the same as the correct answer, then it is correct.
+    // If correct, give a correct message and add to their score. 
+    // If incorrect, give an incorrect message and delete from their score, and take time off of the timer.
+
+
+
+
+
+// game is over when the time reaches 0 or when all the questions are answered
+
+  
+
+
+// page tells them their scores, has them enter their initials
+
+  // after they enter their initials, their initials and score are made into an object
+  // then that object is made into a JSON
+  // then that JSON is stored in local storage
+  // then the JSON is retrieved and shown on the screen with the highest score being at the top
+  // the option is gived to play again or clear high scores
