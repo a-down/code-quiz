85f3ffaf498fa10e87e4ab3f420ca6f072ad02d5
a-down <136649391+a2downing@users.noreply.github.com>
2023-07-06 19:51:32 -0500

    intro elements are removed by the start button

--- assets/script.js
+++ assets/script.js
@@ -6,58 +6,89 @@ var buttonStyling = "width: 100px; height: 25px; background-color: #F08080; bord
 
 // global objects (questions) - test this with 2 questions, so it is quicker to run through the whole thing!
 
-// objectExample = {
-//   choice-a: example,
-//   choice-a: example,
-//   choice-a: example,
-//   choice-a: example,
-//   correct-answer: example,
-// }
-
-
-// function to add elements to the DOM
-function addElement(element, textContent, appendLocation, styling) {
-  var newEl = document.createElement(element);
-  newEl.textContent = textContent;
-  newEl.setAttribute("style", styling);
-  appendLocation.appendChild(newEl);
+var question1 = {
+  question: "What coding language creates the visual components of a webpage?",
+  optionA: "CSS",
+  optionB: "HTML",
+  optionC: "JavaScript",
+  optionD: "Git",
+  correctAnswer: "CSS",
 }
 
-// function to remove elements from the DOM
-function removeElement(element) {
-  var removedElement = document.querySelector(element)
-  removedElement.remove();
+function askAQuestion() {
+  
 }
 
 
-// add text for page greeting
-var introTitle = addElement(
-  "h2", 
-  "Welcome to the quiz!", 
-  mainEl,
-  "font-size: 24px; font-weight: 700; text-align: center;",
-  );
 
-addElement(
-  "p",
-  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum nostrum placeat, quae magnam saepe, excepturi possimus assumenda quisquam eligendi quod voluptate. Libero, cupiditate a porro quia eius officia et labore!",
-  mainEl, 
-  "font-size: 16px; font-weight: 500; text-align: center; margin: auto 5% auto 5%;",
+
+
+
+
+
+// functions to be called throughout
+  // function to add elements to the DOM
+  function addElement(element, textContent, appendLocation, styling) {
+    var newEl = document.createElement(element);
+    newEl.textContent = textContent;
+    newEl.setAttribute("style", styling);
+    appendLocation.appendChild(newEl);
+  }
+
+  // function to remove elements from the DOM
+  function removeElement(element) {
+    var removedElement = document.querySelector(element)
+    removedElement.remove();
+  }
+
+  // function to remove intro elements
+  function removeIntroElements() {
+    removeElement("h2");
+    removeElement("main p");
+    removeElement("button");
+  }
+
+
+
+// add elements for page greeting
+  // intro title
+  addElement(
+    "h2", 
+    "Welcome to the quiz!", 
+    mainEl,
+    "font-size: 24px; font-weight: 700; text-align: center;",
+    );
+
+  // intro paragraph
+  addElement(
+    "p",
+    "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum nostrum placeat, quae magnam saepe, excepturi possimus assumenda quisquam eligendi quod voluptate. Libero, cupiditate a porro quia eius officia et labore!",
+    mainEl, 
+    "font-size: 16px; font-weight: 500; text-align: center; margin: auto 5% auto 5%;",)
+
+  // button to start quiz
+  addElement(
+    "button",
+    "Start",
+    mainEl,
+    buttonStyling,
+  )
+
+  var startButton = document.querySelector("button");
+
+// remove intro elements
+startButton.addEventListener("click", function() {
+  removeIntroElements();
+}
 )
 
 
-// add buttton to start the quiz (style all buttons in CSS)
-addElement(
-  "button",
-  "Start",
-  mainEl,
-  buttonStyling,
-)
 
 
-// remove previous elements
 
 
+
+// console.log(document.body.children[1].children[2])
 
 // code that creates elements to add the questions and answers to <main>, runs after the button is clicked
 
